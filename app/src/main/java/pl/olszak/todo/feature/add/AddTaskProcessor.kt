423452320ad6f9e7ad978a974219a0c0b658e29a
micholszak <michal.olszak@dazn.com>
2021-01-01 22:25:16 +0100

    Remove unused import

--- app/src/main/java/pl/olszak/todo/feature/add/AddTaskProcessor.kt
+++ app/src/main/java/pl/olszak/todo/feature/add/AddTaskProcessor.kt
@@ -3,7 +3,6 @@ package pl.olszak.todo.feature.add
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import pl.olszak.todo.feature.add.interactor.AddTask
-import pl.olszak.todo.feature.data.Task
 import javax.inject.Inject
 
 class AddTaskProcessor @Inject constructor(private val addTask: AddTask) {
