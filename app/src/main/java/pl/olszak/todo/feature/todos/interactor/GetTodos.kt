fa196b7f9255989cf76567c9aa4e01ef1982eeef
micholszak <michal.olszak@dazn.com>
2021-01-27 18:45:13 +0100

    Properly handle todos execution in another scope

--- app/src/main/java/pl/olszak/todo/feature/todos/interactor/GetTodos.kt
+++ app/src/main/java/pl/olszak/todo/feature/todos/interactor/GetTodos.kt
@@ -1,10 +1,12 @@
 package pl.olszak.todo.feature.todos.interactor
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import pl.olszak.todo.core.concurrent.IOCoroutineScope
 import pl.olszak.todo.domain.database.TaskDao
 import pl.olszak.todo.domain.database.model.TaskEntity
+import pl.olszak.todo.feature.data.Task
 import javax.inject.Inject
 
 class GetTodos @Inject constructor(
@@ -12,8 +14,17 @@ class GetTodos @Inject constructor(
     private val scope: IOCoroutineScope
 ) {
 
-    suspend operator fun invoke(): Flow<List<TaskEntity>> =
-        withContext(context = scope.coroutineContext) {
-            taskDao.getAllTasks()
+    operator fun invoke(): Flow<List<Task>> =
+        taskDao.getAllTasks()
+            .map(::mapEntities)
+            .flowOn(scope.coroutineContext)
+
+    private fun mapEntities(entities: List<TaskEntity>): List<Task> =
+        entities.map { entity ->
+            Task(
+                title = entity.title,
+                description = entity.description,
+                priority = entity.priority
+            )
         }
 }
--- app/src/test/java/pl/olszak/todo/feature/todos/interactor/GetTodosTest.kt
+++ app/src/test/java/pl/olszak/todo/feature/todos/interactor/GetTodosTest.kt
@@ -6,10 +6,11 @@ import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 import pl.olszak.todo.core.concurrent.compositeTestCoroutineScope
 import pl.olszak.todo.domain.database.TaskDao
+import pl.olszak.todo.domain.database.model.Priority
 import pl.olszak.todo.domain.database.model.TaskEntity
 
 class GetTodosTest {
@@ -22,13 +23,42 @@ class GetTodosTest {
     )
 
     @Test
-    fun `Retrieve tasks from repository`() = runBlocking {
-        whenever(mockTaskDao.getAllTasks()).doReturn(flowOf(listOf(TaskEntity())))
+    fun `Retrieve tasks from repository`() = runBlockingTest {
+        givenGetAllTasksOperates()
 
         getTodos().test {
-            val entities = expectItem()
-            assertThat(entities).hasSize(1)
+            val tasks = expectItem()
+            assertThat(tasks).hasSize(0)
             expectComplete()
         }
+    }
+
+    @Test
+    fun `Map entities to tasks`() = runBlockingTest {
+        val title = "SCRUM"
+        val description = "Agile framework"
+        val entities = List(5) { index ->
+            TaskEntity(
+                id = index,
+                title = title,
+                description = description,
+                priority = Priority.HIGH
+            )
+        }
+
+        givenGetAllTasksOperates(returnedEntities = entities)
+        getTodos().test {
+            val tasks = expectItem()
+            tasks.forEach { task ->
+                assertThat(task.title).isEqualTo(title)
+                assertThat(task.description).isEqualTo(description)
+                assertThat(task.priority).isEqualTo(Priority.HIGH)
+            }
+            expectComplete()
+        }
+    }
+
+    private fun givenGetAllTasksOperates(returnedEntities: List<TaskEntity> = emptyList()) {
+        whenever(mockTaskDao.getAllTasks()).doReturn(flowOf(returnedEntities))
     }
 }
